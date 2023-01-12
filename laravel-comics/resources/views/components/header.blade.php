0769ca9e05c363ca3a1c327972615a4b932c4d8d
MattiaDG1995 <mattiadigennaro95@gmail.com>
2023-01-12 15:34:06 +0100

    header

---
+++ laravel-comics/resources/views/components/header.blade.php
@@ -0,0 +1,30 @@
+<div class="navblue">
+    <div class="container d-flex justify-content-end text-white">
+        <span class="me-5">DC POWER VISA</span>
+        <span>ADDITIONAL DC SITES</span>
+    </div>
+</div>
+
+
+        <div class="container d-flex justify-content-between align-items-center py-5">
+            <!-- <div>
+                <img src="{{ asset('img/dc-logo.png') }}" alt="">
+            </div> -->
+            <div class="nav">
+                <span>CHARACTERS</span>
+                <span class="blue">COMIX</span>
+                <span>MOVIES</span>
+                <span>TV</span>
+                <span>GAMES</span>
+                <span>COLLECTIBLES</span>
+                <span>VIDEOS</span>
+                <span>FANS</span>
+                <span>NEWS</span>
+                <span>SHOP</span>
+            </div>
+            <div class="search">
+                <span>Search</span>
+            </div>
+        </div>
+  
+
---
+++ laravel-comics/resources/views/layouts/app.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <title>app</title>
+</head>
+<body>
+    <header>
+        @include('components.header')
+    </header>
+    <main>
+        @include('components.main')
+    </main>
+    <footer>
+
+    </footer>
+    
+</body>
+</html>
