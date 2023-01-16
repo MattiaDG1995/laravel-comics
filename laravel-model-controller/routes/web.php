d9ea44cc59bc7856195b43b59a78a3b78f0c462b
MattiaDG1995 <mattiadigennaro95@gmail.com>
2023-01-16 15:48:23 +0100

    stampa dei film a schermo

--- laravel-model-controller/routes/web.php
+++ laravel-model-controller/routes/web.php
@@ -13,6 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/* Route::get('/', function () {
     return view('welcome');
-});
+}); */
+
+Route::get('/', 'HomeController@index')->name('all_movies');
+
+
+
